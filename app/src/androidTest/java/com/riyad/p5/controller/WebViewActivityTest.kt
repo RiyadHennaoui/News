533e58f54a65e85fbe771718c68f6f3cee1f0e28
Riyad <riyad.henna@yahoo.fr>
2020-01-30 09:36:22 +0100

    add webviewtests

--- app/src/androidTest/java/com/riyad/p5/controller/WebViewActivityTest.kt
+++ app/src/androidTest/java/com/riyad/p5/controller/WebViewActivityTest.kt
@@ -1,13 +1,14 @@
 package com.riyad.p5.controller
 
-import androidx.test.core.app.ActivityScenario
+import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
 import com.riyad.p5.R
-import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -16,12 +17,22 @@ import org.junit.runner.RunWith
 class WebViewActivityTest{
 
 
+    @Rule // third parameter is set to false which means the activity is not started automatically
+    @JvmField
+    var mActivityRule: ActivityTestRule<WebViewActivity> =
+        ActivityTestRule<WebViewActivity>(WebViewActivity::class.java, false, false)
+
     @Test
     fun webViewActivity_isDisplyed(){
 
-        val activityScenario = ActivityScenario.launch(WebViewActivity::class.java)
+//        val activityScenario = ActivityScenario.launch(WebViewActivity::class.java)
 
+        val intent = Intent()
+        intent.putExtra(WebViewActivity.EXTRA_ARTICLE_URL, "https://stackoverflow.com/questions/31752303/espresso-startactivity-that-depends-on-intent/45502924")
 
+        mActivityRule.launchActivity(intent)
+
+        Thread.sleep(3000)
         onView(withId(R.id.web_view_activity)).check(matches(isDisplayed()))
 
     }
