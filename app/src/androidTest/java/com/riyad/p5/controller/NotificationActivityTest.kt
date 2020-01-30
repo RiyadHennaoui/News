0bab2d7d4a08ebaa8b2580d8f00a45cf765cf0eb
Riyad <riyad.henna@yahoo.fr>
2020-01-30 18:09:40 +0100

    test Notification Boxes is checked added

--- app/src/androidTest/java/com/riyad/p5/controller/NotificationActivityTest.kt
+++ app/src/androidTest/java/com/riyad/p5/controller/NotificationActivityTest.kt
@@ -3,6 +3,7 @@ package com.riyad.p5.controller
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.matcher.ViewMatchers
@@ -23,6 +24,29 @@ class NotificationActivityTest  {
 
         onView(withId(R.id.notification_activity))
             .check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun Checkboxs_isChecked(){
+        val activityScenario = ActivityScenario.launch(NotificationActivity::class.java)
+
+
+        onView(withId(R.id.search_activity)).check(matches(isDisplayed()))
+        onView(withId(R.id.checkbox1))
+            .perform(ViewActions.click())
+            .check(matches(isChecked()))
+        onView(withId(R.id.checkbox2))
+            .perform(ViewActions.click())
+            .check(matches(isChecked()))
+        onView(withId(R.id.checkbox3))
+            .perform(ViewActions.click())
+            .check(matches(isChecked()))
+        onView(withId(R.id.checkbox4))
+            .perform(ViewActions.click())
+            .check(matches(isChecked()))
+
+
 
     }
 
