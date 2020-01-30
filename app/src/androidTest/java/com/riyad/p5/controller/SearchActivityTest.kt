d1a11c6371ca86ab3496cf65719a231f8c65e914
Riyad <riyad.henna@yahoo.fr>
2020-01-30 15:15:48 +0100

    more tests with boxes checked added

--- app/src/androidTest/java/com/riyad/p5/controller/SearchActivityTest.kt
+++ app/src/androidTest/java/com/riyad/p5/controller/SearchActivityTest.kt
@@ -3,6 +3,7 @@ package com.riyad.p5.controller
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -27,17 +28,38 @@ fun searchActivity_isDisplayed(){
     }
 
     @Test
-    fun buisinessCheckbox_isChecked(){
+    fun Checkboxs_isChecked(){
         val activityScenario = ActivityScenario.launch(SearchActivity::class.java)
 
 
         onView(withId(R.id.search_activity)).check(matches(isDisplayed()))
         onView(withId(R.id.checkbox_1))
             .perform(click())
+            .check(matches(isChecked()))
+        onView(withId(R.id.checkbox_2))
+            .perform(click())
+            .check(matches(isChecked()))
+        onView(withId(R.id.checkbox_3))
+            .perform(click())
+            .check(matches(isChecked()))
+        onView(withId(R.id.checkbox_4))
+            .perform(click())
+            .check(matches(isChecked()))
 
 
 
     }
+
+//    @Test
+//    fun search_Trump() {
+//        val activityScenario = ActivityScenario.launch(SearchActivity::class.java)
+//
+//        onView(withId(R.id.et_search)).perform(typeText("Trump"), closeSoft)
+//        onView(withId(R.id.btn_search)).perform(click())
+//        onView(withText("Trump")).check(matches(isDisplayed()))
+//    }
+
+
 
 
 }
