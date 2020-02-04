4829509ff9e64f9d468e6360d4cf727f96cddbbe
Riyad <riyad.henna@yahoo.fr>
2020-02-04 12:22:01 +0100

    try to comment mostpopularResult line not used

--- app/src/main/java/com/riyad/p5/data/model/MostPopularResult.java
+++ app/src/main/java/com/riyad/p5/data/model/MostPopularResult.java
@@ -7,49 +7,49 @@ import com.google.gson.annotations.SerializedName;
 
 public class MostPopularResult {
 
-    @SerializedName("status")
-    @Expose
-    private String status;
-    @SerializedName("copyright")
-    @Expose
-    private String copyright;
-    @SerializedName("num_results")
-    @Expose
-    private Integer numResults;
+//    @SerializedName("status")
+//    @Expose
+//    private String status;
+//    @SerializedName("copyright")
+//    @Expose
+//    private String copyright;
+//    @SerializedName("num_results")
+//    @Expose
+//    private Integer numResults;
     @SerializedName("results")
     @Expose
     private List<MostPopularArticle> results = null;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getCopyright() {
-        return copyright;
-    }
-
-    public void setCopyright(String copyright) {
-        this.copyright = copyright;
-    }
-
-    public Integer getNumResults() {
-        return numResults;
-    }
-
-    public void setNumResults(Integer numResults) {
-        this.numResults = numResults;
-    }
+//    public String getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+//
+//    public String getCopyright() {
+//        return copyright;
+//    }
+//
+//    public void setCopyright(String copyright) {
+//        this.copyright = copyright;
+//    }
+//
+//    public Integer getNumResults() {
+//        return numResults;
+//    }
+//
+//    public void setNumResults(Integer numResults) {
+//        this.numResults = numResults;
+//    }
 
     public List<MostPopularArticle> getResults() {
         return results;
     }
 
-    public void setResults(List<MostPopularArticle> results) {
-        this.results = results;
-    }
+//    public void setResults(List<MostPopularArticle> results) {
+//        this.results = results;
+//    }
 
 }
