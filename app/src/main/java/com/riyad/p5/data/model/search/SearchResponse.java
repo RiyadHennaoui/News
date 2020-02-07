f36060ea4fd54838f83b6ee8d38df0a3a38ff437
RiyadHennaoui <43846457+RiyadHennaoui@users.noreply.github.com>
2020-02-07 08:08:00 +0100

    remove all comments of SearchResponse

--- app/src/main/java/com/riyad/p5/data/model/search/SearchResponse.java
+++ app/src/main/java/com/riyad/p5/data/model/search/SearchResponse.java
@@ -6,38 +6,12 @@ import com.google.gson.annotations.SerializedName;
 
 public class SearchResponse {
 
-//    @SerializedName("status")
-//    @Expose
-//    private String status;
-//    @SerializedName("copyright")
-//    @Expose
-//    private String copyright;
     @SerializedName("response")
     @Expose
     private Response response;
 
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
-//
-//    public String getCopyright() {
-//        return copyright;
-//    }
-
-//    public void setCopyright(String copyright) {
-//        this.copyright = copyright;
-//    }
-
     public Response getResponse() {
         return response;
     }
-
-//    public void setResponse(Response response) {
-//        this.response = response;
-//    }
 
 }
