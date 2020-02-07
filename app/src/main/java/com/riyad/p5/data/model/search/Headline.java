7c701a5b0a1d900892bd23e6254967dbfedbd469
RiyadHennaoui <43846457+RiyadHennaoui@users.noreply.github.com>
2020-02-07 08:11:08 +0100

    remove all comments of Headline

--- app/src/main/java/com/riyad/p5/data/model/search/Headline.java
+++ app/src/main/java/com/riyad/p5/data/model/search/Headline.java
@@ -9,56 +9,20 @@ public class Headline {
     @SerializedName("main")
     @Expose
     private String main;
-//    @SerializedName("kicker")
-//    @Expose
-//    private Object kicker;
-//    @SerializedName("content_kicker")
-//    @Expose
-//    private Object contentKicker;
     @SerializedName("print_headline")
     @Expose
     private String printHeadline;
     @SerializedName("name")
     @Expose
     private Object name;
-//    @SerializedName("seo")
-//    @Expose
-//    private Object seo;
-//    @SerializedName("sub")
-//    @Expose
-//    private Object sub;
 
     public String getMain() {
         return main;
     }
 
-//    public void setMain(String main) {
-//        this.main = main;
-//    }
-
-//    public Object getKicker() {
-//        return kicker;
-//    }
-//
-//    public void setKicker(Object kicker) {
-//        this.kicker = kicker;
-//    }
-//
-//    public Object getContentKicker() {
-//        return contentKicker;
-//    }
-//
-//    public void setContentKicker(Object contentKicker) {
-//        this.contentKicker = contentKicker;
-//    }
-
     public String getPrintHeadline() {
         return printHeadline;
     }
-
-//    public void setPrintHeadline(String printHeadline) {
-//        this.printHeadline = printHeadline;
-//    }
 
     public Object getName() {
         return name;
@@ -67,21 +31,4 @@ public class Headline {
     public void setName(Object name) {
         this.name = name;
     }
-
-//    public Object getSeo() {
-//        return seo;
-//    }
-//
-//    public void setSeo(Object seo) {
-//        this.seo = seo;
-//    }
-//
-//    public Object getSub() {
-//        return sub;
-//    }
-//
-//    public void setSub(Object sub) {
-//        this.sub = sub;
-//    }
-
 }
