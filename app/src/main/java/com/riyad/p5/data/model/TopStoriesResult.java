e502be9c6f55e7767b2515598c0cf04483e98c3f
RiyadHennaoui <43846457+RiyadHennaoui@users.noreply.github.com>
2020-02-07 07:32:15 +0100

    clean all comments TopStoriesResult

--- app/src/main/java/com/riyad/p5/data/model/TopStoriesResult.java
+++ app/src/main/java/com/riyad/p5/data/model/TopStoriesResult.java
@@ -7,40 +7,13 @@ import com.google.gson.annotations.SerializedName;
 
 public class TopStoriesResult {
 
-//    @SerializedName("status")
-//    @Expose
-//    private String status;
-//    @SerializedName("copyright")
-//    @Expose
-//    private String copyright;
     @SerializedName("section")
     @Expose
     private String section;
-//    @SerializedName("last_updated")
-//    @Expose
-//    private String lastUpdated;
-//    @SerializedName("num_results")
-//    @Expose
-//    private Integer numResults;
+
     @SerializedName("results")
     @Expose
     private List<TopStoriesArticle> topStoriesArticles = null;
-
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
-//
-//    public String getCopyright() {
-//        return copyright;
-//    }
-//
-//    public void setCopyright(String copyright) {
-//        this.copyright = copyright;
-//    }
 
     public String getSection() {
         return section;
@@ -50,28 +23,7 @@ public class TopStoriesResult {
         this.section = section;
     }
 
-//    public String getLastUpdated() {
-//        return lastUpdated;
-//    }
-//
-//    public void setLastUpdated(String lastUpdated) {
-//        this.lastUpdated = lastUpdated;
-//    }
-//
-//    public Integer getNumResults() {
-//        return numResults;
-//    }
-//
-//    public void setNumResults(Integer numResults) {
-//        this.numResults = numResults;
-//    }
-
     public List<TopStoriesArticle> getTopStoriesArticles() {
         return topStoriesArticles;
     }
-
-//    public void setTopStoriesArticles(List<TopStoriesArticle> topStoriesArticles) {
-//        this.topStoriesArticles = topStoriesArticles;
-//    }
-
 }
