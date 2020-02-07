8e13d1b76cd9b922016f04347301aa358e4a13ea
RiyadHennaoui <43846457+RiyadHennaoui@users.noreply.github.com>
2020-02-07 08:51:04 +0100

    remove all coments and ListShouldNotEmpty

--- app/src/androidTest/java/com/riyad/p5/controller/MainActivityTest.kt
+++ app/src/androidTest/java/com/riyad/p5/controller/MainActivityTest.kt
@@ -27,48 +27,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
 
-    // TODO Tester si dans l'onglet home la liste des articles n'est pas vide.
-
-// TODO test
 
     @Rule
     @JvmField
     val mActivityRule = IntentsTestRule<MainActivity>(MainActivity::class.java)
-//    val mActivityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
-
-//    @Before
-//
-//    fun setUp() {
-//
-//        val mActivity = mActivityRule.getActivity()
-//
-//        assertThat(mActivity, notNullValue())
-//
-//    }
-
-//     @Test
-//     fun myListShouldNotBeEmpty() {
-
-//         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-//         Thread.sleep(1000)
-//         onView(
-//             allOf(
-//                 withId(R.id.rv_article),
-//                 isDisplayed()
-//             )
-//         )
-//             .check(matches(hasMinimumChildCount(1)))
-
-//     }
-
-
-    //TODO Tester l'ouverture de la SearchActivity quand on click sur le bouton search
 
     @Test
     fun test_isSearchActivityInView() {
 
-//        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
         onView(withId(R.id.search_btn)).perform(click())
         onView(withId(R.id.search_activity))
@@ -76,25 +43,6 @@ class MainActivityTest {
 
     }
 
-//   @Test
-//   fun test_navNotificationActivity(){
-//
-//       val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-//       onView(withId(R.id.menu_notification)).perform(click())
-//       onView(withId(R.id.notification_activity)).check(matches(isDisplayed()))
-//       intended(IntentMatchers.hasComponent(NotificationActivity::class.java.name))
-//
-//   }
-
-//     @Test
-//     fun test_navSearchActivity() {
-//         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-//
-//         onView(withId(R.id.search_btn)).perform(click())
-//
-//         onView(withId(R.id.search_activity)).check(matches(isDisplayed()))
-//
-//     }
 
     @Test
     fun test_backPress_toMainActivity() {
@@ -109,7 +57,6 @@ class MainActivityTest {
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 
-    //TODO Lorsqu'on clique sur un article qu'il lance la WebViewActivity
 
        @Test
         fun test_articleClicked_toWebView() {
@@ -125,20 +72,6 @@ class MainActivityTest {
 
                .perform(actionOnItemAtPosition<MainAdapter.ViewHolder>(0, click()))
            intended(IntentMatchers.hasComponent(WebViewActivity::class.java.name))
-
-
-
-//         Thread.sleep(1000)
-//
-//         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
-
-//           Thread.sleep(1000)
-//             pressBack()
-//         onView(withId(R.id.main)).check(matches(isDisplayed()))
-     }
-
-
-    //TODO Tester l'ouverture des autres fragements >>> ne pas oublier le NavDrawer
 
 
     @Test
@@ -173,8 +106,6 @@ class MainActivityTest {
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
         onView(withId(R.id.navigation_view))
             .perform(NavigationViewActions.navigateTo(R.id.nav_notification))
-//        onView(withId(R.id.nav_notification))
-//            .perform(click())
        Thread.sleep(1000)
          onView(withId(R.id.notification_activity)).check(matches(isDisplayed()))
         intended(IntentMatchers.hasComponent(NotificationActivity::class.java.name))
@@ -189,11 +120,8 @@ class MainActivityTest {
                 onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
         onView(withId(R.id.navigation_view))
             .perform(NavigationViewActions.navigateTo(R.id.nav_search))
-//        onView(withId(R.id.nav_notification))
-//            .perform(click())
        Thread.sleep(1000)
          onView(withId(R.id.search_activity)).check(matches(isDisplayed()))
-//        intended(IntentMatchers.hasComponent(SearchActivity::class.java.name))
         
     }
 
@@ -207,32 +135,5 @@ class MainActivityTest {
          onView(withId(R.id.main_tl)).check(matches(hasDescendant(withText("Most Popular"))))
 
      }
-
-    @Test
-    fun test_SportListisNotEmpty() {
-
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        Thread.sleep(500)
-        onView(withId(R.id.main_vp_articles)).perform(swipeLeft())
-        onView(withId(R.id.main_tl)).check(matches(hasDescendant(withText("Most Popular"))))
-        onView(withId(R.id.main_vp_articles)).perform(swipeLeft())
-        onView(withId(R.id.main_tl)).check(matches(hasDescendant(withText("BUSINESS"))))
-        onView(withId(R.id.main_vp_articles)).perform(swipeLeft())
-        onView(withId(R.id.main_tl)).check(matches(hasDescendant(withText("SPORTS"))))
-        Thread.sleep(3000)
-
-
-//        onView(allOf(withId(R.id.rv_article), withParent(withId(R.id.main_tl))))
-//                onView(withId(R.id.rv_article))
-//                onData(withId(R.id.item_view))
-//                    .inAdapterView(withId(R.id.rv_article))
-//                    .atPosition(0)
-//                    .perform(click())
-//            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click() ))
-
-
-
-    }
 
 }
