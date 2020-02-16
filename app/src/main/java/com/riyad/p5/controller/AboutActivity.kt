1fb5f9bb38d03a695e66a20463107f4904b75301
Riyad <riyad.henna@yahoo.fr>
2020-02-16 06:14:33 +0100

    About Activity add

---
+++ app/src/main/java/com/riyad/p5/controller/AboutActivity.kt
@@ -0,0 +1,13 @@
+package com.riyad.p5.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyad.p5.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
